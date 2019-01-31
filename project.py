99dc61d85f4cb96067932fb22d99a8b14bbe41ac
qniksefat <qniksefat@gmail.com>
2019-01-31 23:53:05 +0330

    define some classes

--- project.py
+++ project.py
@@ -14,8 +14,31 @@ f_write = open('myfile_copy2.idx', 'wb')
 idx2numpy.convert_to_file(f_write, read_array)
 string = idx2numpy.convert_to_string(read_array)
 
+# Image
+# Label which is just a digit
+
 
 class Image:
-    #
     self = np.array()
 
+
+class Cell:
+    def __init__(self, num):
+        self.num = num
+
+    weights = np.array()
+    output = np.float
+
+# we have Layer(s) in Neural Network which contains some Cells
+class Layer:
+    self = []
+    for i in range(10):
+        x = Cell(i)
+        self.append(x)
+
+
+def announce_output(layer):
+    #todo print the max
+
+def announce_error():
+    #todo the sum of the Squares
