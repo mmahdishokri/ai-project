4766e6e92f5902f0e190090330904426fc00a152
Mahdi <mmahdishokri@gmail.com>
2019-02-03 01:48:57 +0330

    percepron and mira finalized.

--- main.py
+++ main.py
@@ -1,26 +1,57 @@
-from train import train_layer, write_layre_to_json
-from project import Image, get_training_data, Layer
+from train import train_layer, write_layer_to_json, load_layer_from_json, get_output, get_max_and_min_weights
+from project import Image, get_training_data, Layer, get_testing_data
 import numpy as np
 
 
-def start_training_the_layer(layer):
+def start_training_the_layer(layer, algorithm):
     train_images, train_labels = get_training_data()
-    print("Length:", len(train_images))
+    print("Training with " + algorithm + ' algorithm on '+ str(len(train_images)) + " pictures!")
     print("Starting training...")
     for i in range(len(train_images)):
         image = Image()
-        image.pixels = np.ravel(train_images[i])
+        image.pixels = np.ravel(train_images[i])/256
         image.num = int(train_labels[i])
         if i % 600 == 0:
-            print("Training is ", int(i/600), "% completed." )
-        train_layer(layer, image)
+            print("Training is", str(int(i/600)) + "% completed." )
+        train_layer(layer, image, algorithm)
 
 
-layer = Layer()
-print("Layer created!")
-start_training_the_layer(layer)
-print("Training is finished!!")
+def test_trained_data(layer):
+    test_images, test_labels = get_testing_data()
+    error = 0
+    print("Testing on " + str(len(test_images)) + " images!")
+    cnt = 0
+    for i in range(len(test_images)):
+        image = Image()
+        cnt = cnt + 1
+        image.pixels = np.ravel(test_images[i])
+        image.num = int(test_labels[i])
+        if i % 100 == 0:
+            print("Testing is", str(int(i/100)) + "% completed." )
+        guess = get_output(layer, image)
+        if int(guess) != int(test_labels[i]):
+            error = error + 1
+    print("Error = %.2f%%" % (error/cnt*100))
 
-write_layre_to_json('trained-data.json', layer)
 
-print("data is written in json file!")
+testing_mode = True
+training_mode = True
+algorithm = 'mira'
+storage_file_path = 'trained-data-' + algorithm
+
+
+if training_mode:
+    layer = Layer()
+    print("Layer created!")
+    start_training_the_layer(layer, algorithm)
+    print("Training is finished!!")
+    print('Writing trained data to the .json file...')
+    write_layer_to_json(storage_file_path, layer)
+    print("data is written in json file!")
+    mx, mn = get_max_and_min_weights(layer)
+    print("mx = " + str(mx) + ", mn = " + str(mn))
+if testing_mode:
+    print('Reading trained data from the .json file...')
+    layer = load_layer_from_json(storage_file_path)
+    print('Data loaded successfully!')
+    test_trained_data(layer)
--- train.py
+++ train.py
@@ -1,12 +1,14 @@
 import numpy as np
 import json
+from project import Layer, Cell
 
 
 def write_data_to_json(filepath, data):
     with open(filepath + '.json', 'w') as file:
         json.dump(data, file)
 
-def write_layre_to_json(filepath, layer):
+
+def write_layer_to_json(filepath, layer):
     layer_dict = {}
     for i in range(len(layer.cells)):
         cell = layer.cells[i]
@@ -15,6 +17,28 @@ def write_layre_to_json(filepath, layer):
         layer_dict[i]['weights'] = list(cell.weights)
     write_data_to_json(filepath, layer_dict)
 
+
+def load_data_from_json(filepath):
+    with open(filepath + '.json', 'r') as file:
+        return json.load(file)
+
+
+def load_layer_from_json(filepath):
+    layer_dict = load_data_from_json(filepath)
+    layer = Layer()
+    for iid, row in layer_dict.items():
+        cell = Cell(row['num'])
+        cell.weights = row['weights']
+        layer.cells.append(cell)
+    return layer
+
+
+def get_max_and_min_weights(layer):
+    mx, mn = 0, 0
+    for cell in layer.cells:
+        mx = max(mx, max(cell.weights))
+        mn = min(mn, min(cell.weights))
+    return mx, mn
 
 def calc_cell_output(c, im):
     # return 0.5
@@ -27,11 +51,12 @@ def mira_update_cell_weights(l, im, max_cell):
     if im.num == max_cell.num:
         return
     else:
-        learning_rate = 0.01
+        learning_rate = 2
         f = im.pixels
-        taw = (np.dot(max_cell.output - l.cells[im.num].output, f) + 1) / (2 * np.dot(f, f))
+        taw = (np.dot((max_cell.output - l.cells[im.num].output)*(len(im.pixels)), f) + 1) / (2 * np.dot(f, f))
         l.cells[max_cell.num].weights -= learning_rate * taw * f
         l.cells[im.num].weights += learning_rate * taw * f
+        return
 
 
 def perceptron_update_cell_weights(l, im, max_cell):
@@ -39,23 +64,41 @@ def perceptron_update_cell_weights(l, im, max_cell):
     # print(type(l.cells[max_cell.num].weights))
     # print(im.pixels.__sizeof__())
     # print(type(im.pixels))
-    learning_rate = 0.01
-    l.cells[max_cell.num].weights -= learning_rate * im.pixels / 255
-    l.cells[im.num].weights += learning_rate * im.pixels / 255
+    learning_rate = 1
+    l.cells[max_cell.num].weights -= learning_rate * im.pixels
+    l.cells[im.num].weights += learning_rate * im.pixels
     return
     # print()
     # print(l.cells[max_cell.num].weights)
     # print(l.cells[im.num].weights)
 
 
-def train_layer(layer, image):
+
+def kernel_update_cell_weights(l, im, max_cell):
+    # todo
+    return
+
+def get_output(layer, image):
+    max_cell_num = 0
+    max_cell_output = calc_cell_output(layer.cells[0], image)
+    for c in layer.cells:
+        cur = calc_cell_output(c, image)
+        if cur > max_cell_output:
+            max_cell_num = c.num
+            max_cell_output = cur
+    return max_cell_num
+
+
+def train_layer(layer, image, algorithm):
     max_cell = layer.cells[0]
     for c in layer.cells:
         c.output = calc_cell_output(c, image)
         if c.output > max_cell.output:
             max_cell = c
     # print('done the train layer')
-    perceptron_update_cell_weights(layer, image, max_cell)
-    #   mira_update_cell_weights(l, im, max_cell)
-
-    # todo kernel_update_cell_weights
+    if algorithm == 'perceptron':
+        perceptron_update_cell_weights(layer, image, max_cell)
+    if algorithm == 'mira':
+        mira_update_cell_weights(layer, image, max_cell)
+    if algorithm == 'kernel':
+        kernel_update_cell_weights(layer, image, max_cell)
